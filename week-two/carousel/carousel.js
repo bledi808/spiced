807672b92f181287af9dba230920b57228c18a31
Bledi Hasa <bledi.hasa@gmail.com>
2020-09-19 17:42:21 +0200

    updates to carousel files

--- week-two/carousel/carousel.js
+++ week-two/carousel/carousel.js
@@ -1,53 +1,19 @@
 (function () {
     var kitties = document.querySelectorAll(".container img"); //console.log("kitties: ", kitties);
     var dots = document.querySelectorAll(".dot"); //console.log("dots:", dots);
-    var i = 0;
-    // var timer;
     var isCurrentlyAnimating;
-    var j = 0;
+    var i = 0;
+    var timer;
+    // var j = 0;
 
-    var timer = setTimeout(swapKitties, 5000);
+    //call function inside setTimeout
+    timer = setTimeout(swapKitties, 5000);
 
-    // event listener on "transition end" event
-    document.addEventListener("transitionend", function (e) {
-        isCurrentlyAnimating = false;
-        if (e.target.classList.contains("offscreen-left")) {
-            e.target.classList.remove("offscreen-left");
-
-            setTimeout(swapKitties, 5000);
-            console.log("animating?", isCurrentlyAnimating);
-        }
-    });
-
-    // how to grab which dot the user clicked on
-    for (j = 0; j < dots.length; j++) {
-        dots[j].addEventListener("click", function (e) {
-            // clearTimeout(timer);
-            if (j === i) {
-                console.log("same j and i");
-                console.log("j: ", j);
-                console.log("i: ", i);
-                return; // problem 1: if user clicks on dot of the image that is currently displayed, do nothing
-            }
-
-            if (isCurrentlyAnimating) {
-                console.log("hello"); // problem 2: if user clicks dot while images are animating/transitioning, do nothing
-            } else {
-                for (j = 0; j < dots.length; j++) {
-                    if (dots[j] == e.target) {
-                        // dots[j].classList.add("on");
-
-                        // swapKitties(j);
-                        break;
-                    }
-                }
-            }
-        });
-    }
-
+    // main carousel function function
     function swapKitties(nextIndex) {
-        isCurrentlyAnimating = true;
-        console.log("animating?", isCurrentlyAnimating);
+        clearTimeout(timer);
+
+        isCurrentlyAnimating = true; // console.log("animating:", isCurrentlyAnimating);
         kitties[i].classList.remove("onscreen");
         kitties[i].classList.add("offscreen-left");
         dots[i].classList.remove("on");
@@ -55,8 +21,7 @@
         if (typeof nextIndex != "undefined") {
             i = nextIndex;
         } else {
-            i++;
-            console.log[i];
+            i++; //console.log[i];
 
             if (i === kitties.length) {
                 i = 0;
@@ -65,5 +30,36 @@
             kitties[i].classList.add("onscreen");
             dots[i].classList.add("on");
         }
+    }
+
+    // event listener on "transition end" event - places offscreen-left cat back in queue
+    document.addEventListener("transitionend", function (e) {
+        isCurrentlyAnimating = false; //console.log("animating:", isCurrentlyAnimating);
+        if (e.target.classList.contains("offscreen-left")) {
+            e.target.classList.remove("offscreen-left");
+            setTimeout(swapKitties, 5000);
+        }
+    });
+
+    // event listener on dot clicks; making the dots clickable
+    for (var j = 0; j < dots.length; j++) {
+        dots[j].addEventListener("click", function (e) {
+            clearTimeout(timer);
+            if (isCurrentlyAnimating) {
+                return; // console.log("in transition"); problem 1: dot clicks during transition
+            }
+            for (j = 0; j < dots.length; j++) {
+                if (dots[j] == e.target) {
+                    if (j == i) {
+                        return; // console.log("same j and i");problem 2: clicks on current dot
+                    }
+                    dots[j].classList.add("on");
+                    kitties[j].classList.add("onscreen");
+                    // the time out is the only thing not working correctly; it is not reseting once clicked on image appears
+                    swapKitties(j); // setTimeout(swapKitties(j), 10000);
+                    break;
+                }
+            }
+        });
     }
 })();
