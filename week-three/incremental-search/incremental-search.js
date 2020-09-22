2d80a5440be4b21524821c8aef7d915008a8e19d
Bledi Hasa <bledi.hasa@gmail.com>
2020-09-22 22:57:12 +0200

    uploads incremental search files

---
+++ week-three/incremental-search/incremental-search.js
@@ -0,0 +1,308 @@
+(function (countries) {
+    // console.log($);
+    // console.log(countries);
+
+    var searchField = $("input");
+    // console.log("search: ", searchField);
+
+    var displayResults = $(".results");
+
+    var i = 0;
+    // input
+    searchField.on("input", function () {
+        // console.log("sanity input");
+        // console.log("searchField.val():", searchField.val());
+        var userInput = searchField.val().toLowerCase();
+        var results = [];
+        for (var i = 0; i < countries.length; i++) {
+            // console.log("check:", countries[i]);
+            if (countries[i].toLowerCase().indexOf(userInput) == 0) {
+                results.push(countries[i]);
+                // console.log(results);
+            }
+            if (results.length === 4) {
+                break;
+            }
+        }
+        // console.log("countries:", results);
+        var htmlForCountries = "";
+        for (i = 0; i < results.length; i++) {
+            htmlForCountries += "<p class='country'>" + results[i] + "</p>";
+            console.log(htmlForCountries);
+            displayResults.html(htmlForCountries);
+            displayResults.css({
+                width: "23vw",
+                fontSize: "16px",
+                cursorType: "default",
+            });
+        }
+
+        //handles case for when input is cleared, results are cleared
+        if (userInput.length == 0) {
+            displayResults.hide();
+        } else {
+            displayResults.show();
+        }
+        //handles case of no matching results but there is user input
+        var htmlForNoResults = "<p>" + "No countries found!" + "</p>";
+        if (results.length === 0 && userInput.length !== 0) {
+            // console.log("No matching");
+            // console.log(htmlForNoResults);
+            displayResults.html(htmlForNoResults);
+            displayResults.css({
+                color: "lightslategray",
+                // transform: "translateY(-100%)",
+            });
+        }
+    });
+
+    searchField.on("input", function () {});
+
+    // blur listener
+    searchField.on("blur", function () {
+        displayResults.hide();
+    });
+
+    //focus listener
+    searchField.on("focus", function () {
+        displayResults.show();
+    });
+
+    // mouseover, using event delegation
+    displayResults.on("mouseover", ".country", function (e) {
+        $(".highlight").removeClass("highlight");
+        $(e.target).addClass("highlight");
+    });
+
+    // mousedown - no need for delegation as it bubbles up to searchField
+    displayResults.on("mousedown", ".country", function (e) {
+        searchField.val($(".highlight").text());
+    });
+
+    // searchField.on("keydown", function (e) {
+    //     if (e.keyCode === 40) {
+    //         $(".highlight").removeClass("highlight");
+    //         $("p").eq(i).addClass("highlight");
+    //         i++;
+    //         console.log(i);
+    //     }
+    //     if (i === 4) {
+    //         i = 0;
+    //     }
+    // });
+
+    searchField.on("keydown", function (e) {
+        if (e.keyCode === 40) {
+            if (!$("p").hasClass("highlight")) {
+                $("p").first().addClass("highlight");
+            }
+            // $("p").next().addClass("highlight").prev().removeClass("highlight");
+
+            // if ($("p").eq(0).hasClass("highlight")) {
+            //     $(".highlight").removeClass("highlight");
+            //     $("p").eq(1).addClass("highlight");
+        }
+        if (e.keyCode === 13) {
+            if ($("p").first().hasClass("highlight")) {
+                //do nothing
+            }
+        }
+        if (e.keyCode === 13) {
+            searchField.val($(".highlight").text());
+        }
+    });
+})([
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Brazil",
+    "Brunei Darussalam",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cabo Verde",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Colombia",
+    "Comoros",
+    "Congo",
+    "Costa Rica",
+    "Côte D'Ivoire",
+    "Croatia",
+    "Cuba",
+    "Cyprus",
+    "Czech Republic",
+    "Democratic People's Republic of Korea",
+    "Democratic Republic of the Congo",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Eswatini",
+    "Ethiopia",
+    "Fiji",
+    "Finland",
+    "France",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Greece",
+    "Grenada",
+    "Guatemala",
+    "Guinea",
+    "Guinea Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Lao People’s Democratic Republic",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Micronesia",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "North Macedonia",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Qatar",
+    "Republic of Korea",
+    "Republic of Moldova",
+    "Romania",
+    "Russian Federation",
+    "Rwanda",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "Suriname",
+    "Sweden",
+    "Switzerland",
+    "Syrian Arab Republic",
+    "Tajikistan",
+    "Thailand",
+    "Timor-Leste",
+    "Togo",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United Republic of Tanzania",
+    "United States of America",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Venezuela",
+    "Viet Nam",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+]);
