79f151ffc90cb360f6069ec0815521eca9c328ae
Bledi Hasa <bledi.hasa@gmail.com>
2020-10-15 12:22:16 +0200

    updates actors.sql file

--- week-six/sql/actors.sql
+++ week-six/sql/actors.sql
@@ -8,20 +8,20 @@ CREATE TABLE actors  (
     id SERIAL PRIMARY KEY,
     name VARCHAR(250) NOT NULL,
     age INT,
-    oscars INT
+    "number of oscars" INT
 );
 
 -- terminal: access database and and table: psql actors-exercise and \dt 
 -- terminal: run actors.sql file to insert the below rows into actors table: psql -d actors-exercise -f actors.sql
 
-INSERT INTO actors (name, age, oscars) VALUES ('Leonardo DiCaprio', '41', 1);
-INSERT INTO actors (name, age, oscars) VALUES ('Jennifer Lawrence	', '25', 1);
-INSERT INTO actors (name, age, oscars) VALUES ('Samuel L. Jackson', '67', 0);
-INSERT INTO actors (name, age, oscars) VALUES ('Meryl Streep', '66', 3);
-INSERT INTO actors (name, age, oscars) VALUES ('John Cho', '43', 0);
+INSERT INTO actors (name, age, "number of oscars") VALUES ('Leonardo DiCaprio', '41', 1);
+INSERT INTO actors (name, age, "number of oscars") VALUES ('Jennifer Lawrence	', '25', 1);
+INSERT INTO actors (name, age, "number of oscars") VALUES ('Samuel L. Jackson', '67', 0);
+INSERT INTO actors (name, age, "number of oscars") VALUES ('Meryl Streep', '66', 3);
+INSERT INTO actors (name, age, "number of oscars") VALUES ('John Cho', '43', 0);
 
 -- terminal: access database and and table: psql actors-exercise and \dt and SELECT * FROM actors; (returns all rows)
--- terminal: SELECT actors FROM actors WHERE oscars > 1; 
+-- terminal: SELECT actors FROM actors WHERE "number of oscars" > 1; 
 ----------returns (4,"Meryl Streep",66,3)
 
 -- terminal: SELECT actors FROM actors WHERE age > 30; returs:
